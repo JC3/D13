9937d5ae30c72e27dcefa27ba83971a14d3823fe
JasonC <jason.cipriani@gmail.com>
2017-07-05 11:23:27 -0400

    1.23f
    - Improved phone number handling (uses libphonenumber now).
    - International cell numbers allowed.
    - Proper phone number validation.
    - Reg data now also displays what to dial for international numbers from
    US.
    - New phone summary/check tool added to secret tools in /misc.

---
+++ WebContent/misc/phone.jsp
@@ -0,0 +1,192 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%@ page import="java.util.*" %>
+<%@ page import="d13.web.SessionData" %>
+<%@ page import="d13.dao.*" %>
+<%@ page import="d13.util.Util" %>
+<%@ page import="com.google.i18n.phonenumbers.PhoneNumberUtil.PhoneNumberFormat" %>
+<%!
+private static boolean isValidNumber (String s) {
+    try {
+        Util.parsePhoneNumber(s);
+        return true;
+    } catch (Exception x) {
+        return false;
+    }    
+}
+private static String validateNumber (String s) {
+    try {
+        Util.PhoneNumberInfo info = Util.parsePhoneNumber(s);
+        return info == null ? null : info.formatted;
+    } catch (Exception x) {
+        return s;
+    }
+}
+private static String formatNumber (String s, PhoneNumberFormat f) {
+    try {
+        return Util.formatPhoneNumber(s, f);
+    } catch (Exception x) {
+        return null;
+    }
+}
+private static String formatNumberMobile (String s) {
+    try {
+        return Util.formatPhoneNumberMobile(s);
+    } catch (Exception x) {
+        return null;
+    }
+}
+private static String getRegion (String s) {
+    try {
+        return Util.getPhoneNumberRegion(s);
+    } catch (Exception x) {
+        return null;
+    }
+}
+
+private static void doReparse () {
+    for (User user : User.findAll()) {
+        try {
+            user.setPhone(user.getPhone());
+        } catch (Exception x) {
+            System.out.printf("REPARSE: %s (%s) failed for '%s': %s\n", 
+                    user.getRealName(),
+                    user.getUserId(),
+                    user.getPhone(),
+                    x.getMessage());
+        }
+    }
+}
+
+private static String contactNumbers (User u) {
+    List<String> ns = Util.getPhoneNumbers(u.getEmergencyContact());
+    StringBuilder b = new StringBuilder();
+    for (String n : ns) {
+        if (b.length() > 0)
+            b.append(", ");
+        b.append(n);
+    }
+    return b.toString();
+}
+%>
+<%
+SessionData sess = new SessionData(session);
+if (!sess.isLoggedIn()) {
+    sess.setAttribute(SessionData.SA_LOGIN_ERROR, "You must be logged in.");
+    response.sendRedirect(this.getServletContext().getContextPath() + "/index.jsp?next=" + java.net.URLEncoder.encode(Util.getCompleteUrl(request), "us-ascii"));
+    return;
+}
+
+User editor = sess.getUser();
+if (!editor.getRole().canEditUsers())
+    return;
+
+String action = request.getParameter("do");
+if ("reparse".equals(action)) {
+    doReparse();
+    return;
+}
+
+List<User> users = new ArrayList<User>();
+for (User user : User.findAll("realName"))
+    if (user.getState() != UserState.NEW_USER)
+        users.add(user);
+
+String detail = Util.getAbsoluteUrl(request, "details.jsp") + "?u=";
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Phone Number Tool</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function () {
+	$('#reparse-all').click(function () {
+		if (!confirm('This could break stuff. Are you sure?'))
+			return false;
+		$.get('phone.jsp?do=reparse').then(function () {
+			window.location.reload();
+		}).fail(function () {
+			alert('Something went wrong.');
+		});
+	});
+});
+</script>
+<style type="text/css">
+.numbers {
+    border-collapse: collapse;
+}
+.numbers td {
+    border: 1px solid #aaa;
+    padding-left: 1ex;
+    padding-right: 1ex;
+    vertical-align: top;
+    white-space: nowrap;
+    text-align: right;
+}
+.numbers th {
+    font-weight: bold;
+    background: #eee;
+}
+.numbers td:nth-child(1) {
+    text-align: left;
+}
+.invalid {
+    background: #fdd;
+}
+.actions {
+    padding: 1ex;
+    border: 1px solid #aaa;
+    margin-bottom: 1ex;
+}
+</style>
+</head>
+<body>
+<div class="actions">
+Actions:
+<button id="reparse-all">Reparse Numbers</button>
+</div>
+<h1>Phone Numbers</h1>
+<table class="numbers">
+<tr>
+  <th>Name
+  <th>Number
+  <th>Validated
+  <th>From US (db)
+  <th>Region
+  <th>From US
+  <th>From Mobile
+  <th>E164
+  <th>RFC3966
+  <th>International
+  <th>National
+<% for (User u : users) { %>
+<tr<%= isValidNumber(u.getPhone()) ? "" : " class=\"invalid\"" %>>
+  <td><a href="<%= detail + u.getUserId() %>"><%= Util.html(u.getRealName()) %></a>
+  <td><%= Util.html(u.getPhone()) %>
+  <td><%= Util.html(validateNumber(u.getPhone())) %>
+  <td><%= Util.html(u.getPhoneFromUs()) %>
+  <td><%= Util.html(getRegion(u.getPhone())) %>
+  <td><%= Util.html(formatNumber(u.getPhone(), null)) %>
+  <td><%= Util.html(formatNumberMobile(u.getPhone())) %>
+  <td><%= Util.html(formatNumber(u.getPhone(), PhoneNumberFormat.E164)) %>
+  <td><%= Util.html(formatNumber(u.getPhone(), PhoneNumberFormat.RFC3966)) %>
+  <td><%= Util.html(formatNumber(u.getPhone(), PhoneNumberFormat.INTERNATIONAL)) %>
+  <td><%= Util.html(formatNumber(u.getPhone(), PhoneNumberFormat.NATIONAL)) %>
+<% } %>
+</table>
+<h1>Emergency Contacts</h1>
+<table class="numbers">
+<tr>
+  <th>Name
+  <th>Contact Text
+  <th>Contact Numbers
+<% for (User u : users) {
+    String c = contactNumbers(u); %>
+<tr<%= c.isEmpty() ? " class=\"invalid\"" : "" %>>
+  <td><a href="<%= detail + u.getUserId() %>"><%= Util.html(u.getRealName()) %></a>
+  <td><%= Util.html(u.getEmergencyContact()) %>
+  <td><%= Util.html(c) %>
+<% } %>
+</table>
+</body>
+</html>
--- src/d13/util/Util.java
+++ src/d13/util/Util.java
@@ -3,12 +3,27 @@ package d13.util;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.mail.internet.InternetAddress;
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.StringEscapeUtils;
+
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberMatch;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.PhoneNumberUtil.PhoneNumberFormat;
+import com.google.i18n.phonenumbers.Phonenumber.PhoneNumber;
+
+import d13.dao.User;
+import d13.dao.UserState;
 
 public class Util {
     
@@ -120,17 +135,45 @@ public class Util {
         return str;
     }
     
-    public static String validatePhoneNumber (String s) {
-        // only u.s. 10 digit numbers are valid for now
-        StringBuilder pn = new StringBuilder();
-        for (int n = 0; n < s.length(); ++ n)
-            if (Character.isDigit(s.charAt(n)))
-                pn.append(s.charAt(n));
-        if (pn.length() != 10 && pn.length() != 0) // either 10-digit, or none
-            throw new IllegalArgumentException("Phone number, if specified, must be a 10-digit US phone number.");
-        return pn.toString();
+    private static Object pnMutex = new Object();
+    
+    public static class PhoneNumberInfo {
+        public String raw;
+        public String formatted;
+        public String normalized;
+        public String fromUS;
+        public String region;
     }
     
+    public static PhoneNumberInfo parsePhoneNumber (String s) {
+        
+        if (s == null || s.trim().isEmpty())
+            return null;
+        
+        // Unsure if PhoneNumberUtil is thread-safe, so just assume not.
+        synchronized (pnMutex) {
+            try {
+                PhoneNumberUtil pnu = PhoneNumberUtil.getInstance();
+                PhoneNumber pn = pnu.parseAndKeepRawInput(s, "US");
+                if (!pnu.isValidNumber(pn))
+                    throw new IllegalArgumentException("Specified phone number does not appear to be valid. If it is a non-US number please be sure to include the country code (possibly with a + sign in front).");
+                PhoneNumberInfo info = new PhoneNumberInfo();
+                info.raw = s.trim();
+                info.formatted = pnu.formatInOriginalFormat(pn, "US");
+                info.normalized = PhoneNumberUtil.normalizeDigitsOnly(info.formatted);
+                info.region = pnu.getRegionCodeForNumber(pn);
+                if ("US".equals(info.region) || "CA".equals(info.region))
+                    info.fromUS = info.formatted.replaceAll("[^0-9]+", " ").trim();
+                else
+                    info.fromUS = pnu.formatOutOfCountryCallingNumber(pn, "US");
+                return info;
+            } catch (NumberParseException x) {
+                throw new IllegalArgumentException(x.getMessage(), x);
+            }
+        }
+        
+    }
+        
     public static String randomString (int length) {
         StringBuilder sb = new StringBuilder();
         for (int n = 0; n < length; ++ n) {
@@ -197,5 +240,141 @@ public class Util {
         return ip;
         
     }  
+    
+    
+    //=============================================================================================
+    // HACKY INTERNAL STUFF, DO NOT USE IN MAIN APP
+    //=============================================================================================
+    
+    /** For phone.jsp use only! */
+    public static String formatPhoneNumber (String s, PhoneNumberFormat fmt) {
+        if (s == null || s.trim().isEmpty())
+            return "";
+        synchronized (pnMutex) {
+            try {
+                PhoneNumberUtil pnu = PhoneNumberUtil.getInstance();
+                PhoneNumber pn = pnu.parseAndKeepRawInput(s, "US");
+                if (!pnu.isValidNumber(pn))
+                    throw new IllegalArgumentException();
+                if (fmt == null)
+                    return pnu.formatOutOfCountryCallingNumber(pn, "US");
+                else
+                    return pnu.format(pn, fmt);
+            } catch (NumberParseException x) {
+                throw new IllegalArgumentException(x.getMessage(), x);
+            }
+        }
+    }
+    
+    /** For phone.jsp use only! */
+    public static String formatPhoneNumberMobile (String s) {
+        if (s == null || s.trim().isEmpty())
+            return "";
+        synchronized (pnMutex) {
+            try {
+                PhoneNumberUtil pnu = PhoneNumberUtil.getInstance();
+                PhoneNumber pn = pnu.parseAndKeepRawInput(s, "US");
+                if (!pnu.isValidNumber(pn))
+                    throw new IllegalArgumentException();
+                return pnu.formatNumberForMobileDialing(pn, "US", true);
+            } catch (NumberParseException x) {
+                throw new IllegalArgumentException(x.getMessage(), x);
+            }
+        }
+    }
+    
+    /** For phone.jsp use only! */
+    public static String getPhoneNumberRegion (String s) {
+        if (s == null || s.trim().isEmpty())
+            return "";
+        synchronized (pnMutex) {
+            try {
+                PhoneNumberUtil pnu = PhoneNumberUtil.getInstance();
+                PhoneNumber pn = pnu.parseAndKeepRawInput(s, "US");
+                String code = pnu.getRegionCodeForNumber(pn);
+                return code == null ? "" : code;
+            } catch (NumberParseException x) {
+                throw new IllegalArgumentException(x.getMessage(), x);
+            }
+        }
+    }
+    
+    /** For phone.jsp use only! */
+    public static List<String> getPhoneNumbers (String s) {
+        List<String> numbers = new ArrayList<String>();
+        synchronized (pnMutex) {
+            PhoneNumberUtil pnu = PhoneNumberUtil.getInstance();
+            try {
+                for (PhoneNumberMatch pnm : pnu.findNumbers(s, "US")) {
+                    //PhoneNumber pn = pnm.number();
+                    PhoneNumber pn = pnu.parseAndKeepRawInput(pnm.rawString(), "US");
+                    String pns = pnu.formatInOriginalFormat(pn, "US");
+                    numbers.add(pns);
+                }
+            } catch (Exception x) {
+                throw new IllegalArgumentException(x.getMessage(), x);
+            }
+        }
+        return numbers;
+    }
+    
+    /** For check.jsp use only! */
+    public static class PhoneCheckData {
+        private final Map<PhoneNumber,List<Long>> numbers = new HashMap<PhoneNumber,List<Long>>();
+        private final Map<Long,List<PhoneNumber>> contacts = new HashMap<Long,List<PhoneNumber>>();
+        public PhoneCheckData (Collection<User> users) {
+            synchronized (pnMutex) {
+                PhoneNumberUtil pnu = PhoneNumberUtil.getInstance();
+                for (User u : users) {
+                    if (u.getState() == UserState.NEW_USER)
+                        continue;
+                    try {
+                        PhoneNumber pn = pnu.parseAndKeepRawInput(u.getPhone(), "US");
+                        List<Long> uids = numbers.get(pn);
+                        if (uids == null) {
+                            uids = new ArrayList<Long>();
+                            numbers.put(pn, uids);
+                        }
+                        uids.add(u.getUserId());
+                    } catch (Exception x) {
+                    }
+                    try {
+                        List<PhoneNumber> contactpn = new ArrayList<PhoneNumber>();
+                        for (PhoneNumberMatch pnm : pnu.findNumbers(u.getEmergencyContact(), "US")) {
+                            PhoneNumber pn = pnu.parseAndKeepRawInput(pnm.rawString(), "US");
+                            contactpn.add(pn);
+                        }
+                        contacts.put(u.getUserId(), contactpn);
+                    } catch (Exception x) {
+                    }
+                }
+            }
+        }
+        public static class Match {
+            public String thisPhone;
+            public User otherUser;
+        }
+        public List<Match> getECMatches (User u) {
+            List<PhoneNumber> thisContacts = contacts.get(u.getUserId());
+            if (thisContacts == null || thisContacts.isEmpty())
+                return Collections.emptyList();
+            List<Match> matches = new ArrayList<Match>();
+            synchronized (pnMutex) {
+                PhoneNumberUtil pnu = PhoneNumberUtil.getInstance();
+                for (PhoneNumber thisContact : thisContacts) {
+                    List<Long> otherUsers = numbers.get(thisContact);
+                    if (otherUsers == null)
+                        continue;
+                    for (Long uid : otherUsers) {                        
+                        Match match = new Match();
+                        match.thisPhone = pnu.formatInOriginalFormat(thisContact, "US");
+                        match.otherUser = User.findById(uid);
+                        matches.add(match);
+                    }
+                }
+            }
+            return matches;
+        }
+    }
     
 }
